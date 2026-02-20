37bc19b35f97f6e11a7125c1791f031d3350fec1
Daniela <alfaroguadalupe015@gmail.com>
2026-02-20 16:59:21 -0600

    Primer commit: app de contactos inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContactList from "./components/ContactList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi Agenda de Contactos</h1>
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
